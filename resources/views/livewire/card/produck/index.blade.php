8de71b7ccc4d3ecb2c844271cbba5da3c7609d0e
Muhammad Iqbal Ainu Rafie <74098172+ainurafie@users.noreply.github.com>
2023-04-01 22:18:44 +0700

    card product

---
+++ resources/views/livewire/card/produck/index.blade.php
@@ -0,0 +1,25 @@
+<div>
+    <div class="w-[278px] rounded overflow-hidden shadow-lg">
+        <img class="w-full" src="../assets/images/marsha.png" alt="Sunset in the mountains">
+        <div class="px-6 py-4">
+            <div class="font-bold text-lg font-montserrat mb-2 line-clamp-1">Elforia Crochet</div>
+            <p class="text-[#919191] text-xs mb-5 line-clamp-1">Tas Rajut Kecil
+            </p>
+            <h1 class="text-[#CB3A26] text-base font-montserrat font-medium">Rp43.000</h1>
+            <div class="flex justify-between mt-3 items-center">
+                <div class="w-full">
+                    <h1 class="text-xs text-[#919191] font-montserrat">Banyumas</h1>
+                </div>
+                <div class="w-full border-l border-[#cfcfcf] px-6 gap-2 flex items-center">
+                    <svg width="20" height="20" viewBox="0 0 20 20" fill="none"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path
+                            d="M3.825 19.5167L5.45 12.4917L0 7.76672L7.2 7.14172L10 0.516724L12.8 7.14172L20 7.76672L14.55 12.4917L16.175 19.5167L10 15.7917L3.825 19.5167Z"
+                            fill="#FFDD2A" />
+                    </svg>
+                    <h1 class="text-sm text-[#919191] font-montserrat">4.9</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
