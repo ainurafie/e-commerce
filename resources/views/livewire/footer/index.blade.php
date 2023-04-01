ffa852190b18000d3758061ade79f6784567228e
Muhammad Iqbal Ainu Rafie <74098172+ainurafie@users.noreply.github.com>
2023-04-01 22:18:28 +0700

    footer

---
+++ resources/views/livewire/footer/index.blade.php
@@ -0,0 +1,10 @@
+<div>
+    <footer class="bg-[#2AA345]">
+        <div class="w-full mx-auto max-w-screen-xl p-4 justify-center flex items-center">
+            <span class="text-sm text-white sm:text-center ">© Hersa Dwi Ikhsanti
+            </span>
+          
+        </div>
+    </footer>
+
+</div>
