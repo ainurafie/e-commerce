c020eea2a540375689b47b11bb751871d5bcc0d5
Muhammad Iqbal Ainu Rafie <74098172+ainurafie@users.noreply.github.com>
2023-06-17 13:34:40 +0700

    fixing profile

--- resources/views/profileUser.blade.php
+++ resources/views/profileUser.blade.php
@@ -13,10 +13,134 @@
         rel="stylesheet">
     <script src="https://cdn.tailwindcss.com"></script>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.css" rel="stylesheet" />
-    <title>Profile</title>
+    <title>UMKM Banyumasan</title>
     @vite('resources/css/app.css')
 
 </head>
 
 <body>
     <livewire:navbar.index />
+    <div class="md:px-24 mt-48">
+        <h1 class="text-xl font-montserrat font-semibold px-6 mb-12">Profile</h1>
+        <div class="bg-white rounded-xl py-[17px] px-6 ">
+            <div class="flex justify-center">
+                <div class="flex gap-5 justify-center">
+                    <div class="relative">
+                        <label type="file" name="image-product" id="image-product" required>
+                            <span class="">
+                                <img class="img-preview w-32 h-32 border-2 rounded-full hover:bg-slate-200 cursor-pointer"
+                                    alt="">
+                            </span>
+                            <input class="hidden" type="file" name="images" id="image"
+                                onchange="previewImage()">
+                        </label>
+                    </div>
+                    <div class="w-3/4">
+                        <div class="py-4 bg-white shadow drop-shadow-lg rounded-[8px] mb-6">
+                            <div class="px-6">
+                                <h1 class="font-semibold text-sm font-montserrat text-black mb-5">Informasi Pribadi</h1>
+                            </div>
+                            <table>
+                                <tbody>
+                                    <tr class="">
+                                        <td class="px-6 py-4 truncate">
+                                            Nama
+                                        </td>
+                                        <td class="px-6 py-4 ">
+                                            Hersa Dwi Ikhsanti</td>
+                                    </tr>
+                                    <tr class="">
+                                        <td class="px-6 py-4 truncate">
+                                            Tanggal Lahir
+                                        </td>
+                                        <td class="px-6 py-4 ">
+                                            10 Agustus 2000</td>
+                                    </tr>
+                                    <tr class="">
+                                        <td class="px-6 py-4 truncate">
+                                            Jenis Kelamin
+                                        </td>
+                                        <td class="px-6 py-4 ">
+                                            Perempuan</td>
+                                    </tr>
+                                    <tr class="">
+                                        <td class="px-6 py-4 truncate">
+                                            Alamat
+                                        </td>
+                                        <td class="px-6 py-4 ">
+                                            Alun-Alun, Regol Wetan, Kec. Sumedang Sel., Kabupaten Sumedang, Jawa Barat
+                                            45311</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                        <div class="py-4 bg-white shadow drop-shadow-lg rounded-[8px] mb-6">
+                            <div class="px-6">
+                                <h1 class="font-semibold text-sm font-montserrat text-black mb-5">Akun</h1>
+                            </div>
+                            <table>
+                                <tbody>
+                                    <tr class="">
+                                        <td class="px-6 py-4 truncate">
+                                            Username
+                                        </td>
+                                        <td class="px-6 py-4 truncate">
+                                            @hersadwi</td>
+                                    </tr>
+                                    <tr class="">
+                                        <td class="px-6 py-4 truncate">
+                                            Email
+                                        </td>
+                                        <td class="px-6 py-4 truncate">
+                                            hersadwiikhsanti@email.com</td>
+                                    </tr>
+                                    <tr class="">
+                                        <td class="px-6 py-4 truncate">
+                                            Password
+                                        </td>
+                                        <td class="px-6 py-4 truncate">
+                                            ******</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <livewire:footer.index />
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.js"></script>
+    <script>
+        var mainNav = document.getElementById("main-nav");
+        var toggleMenuIcon = document.getElementById("toggle-menu-icon");
+
+        function toggleMenu() {
+            mainNav.classList.toggle('hidden');
+
+            // change the icon when the menu is shown/closed
+            toggleMenuIcon.classList.toggle('fa-bars');
+            toggleMenuIcon.classList.toggle('fa-times');
+        }
+    </script>
+    <script>
+        function previewImage() {
+
+            const image = document.querySelector('#image');
+            const imgPreview = document.querySelector('.img-preview');
+
+            imgPreview.style.display = 'block';
+
+            const oFReader = new FileReader();
+
+            oFReader.readAsDataURL(image.files[0]);
+            oFReader.onload = function(oFREvent) {
+                imgPreview.src = oFREvent.target.result;
+            }
+        }
+    </script>
+</body>
+
+</html>
