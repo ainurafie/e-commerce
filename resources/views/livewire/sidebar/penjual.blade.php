754c19ff04d9003a27eabc2f0b9810a741e9f9f2
adnan <adnanega82@gmail.com>
2023-07-16 17:29:31 +0700

    bantuan

--- resources/views/livewire/sidebar/penjual.blade.php
+++ resources/views/livewire/sidebar/penjual.blade.php
@@ -112,6 +112,24 @@
                             <span class="flex-1 ml-3 whitespace-nowrap">Produk</span>
                         </a>
                     </li>
+                    <li>
+                        <a href="/bantuan"
+                            class="flex items-center p-2 text-white rounded-lg hover:bg-green-700">
+                            <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke-width="1.5"
+                            stroke="currentColor"
+                            class="h-8 w-8">
+                            <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z" />
+                              </svg>
+                            <span class="flex-1 ml-3 whitespace-nowrap">Bantuan</span>
+                        </a>
+                    </li>
                 </ul>
             </div>
             <div class="flex px-5 py-1.5 gap-3 rounded-[8px] bg-[#70B17F] items-center justify-center">
